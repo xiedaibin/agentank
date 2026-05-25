8c613ba24dc1060949430eb3bd1a0e60eb2218c2
xiedaibin <xieidaibin@sina.cn>
2026-05-25 14:11:24 +0800

    调整

--- test_syntax.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const code = fs.readFileSync('new_tank.js', 'utf8');
-const vm = require('vm');
-try {
-  new vm.Script(`function __wrapper__() {\n${code}\n}`);
-  console.log('OK');
-} catch (e) {
-  console.error(e.stack);
-}
